96f1bb12f39a6e25f8f1d67615b324395010c9c7
Dushusir <1414556676@qq.com>
2020-10-19 23:26:52 +0800

    npm

--- deploy.bat
+++ deploy.bat
@@ -3,3 +3,8 @@ npm run docs:build
 git add docs/.vuepress/dist
 git commit -m "Initial dist subtree commit"
 git subtree push --prefix docs/.vuepress/dist origin gh-pages
+
+# npm publish
+npm login
+npm version patch
+npm publish
